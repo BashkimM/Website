290e39e8417ff0c822ab06db9c3b4def0f6bf9b7
BashkimM <bashkim_mehmeti@gmx.de>
2021-10-01 23:23:25 +0200

    another try

--- placeorder.php
+++ placeorder.php
@@ -29,7 +29,7 @@ if(isset($_SESSION['username'])){
     $loggedin = false;
 }
  
-// Check if the user is already logged in, if not then redirect him to login page
+// Check if the user is already      logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] != true ) {
     header("location: Login.php");
     exit;
